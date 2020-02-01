2fa410db8b57a277d22cc76333dc841560a9d923
ibecerra4 <ibecerra4@miners.utep.edu>
2020-02-01 10:30:07 -0700

    Committing button

---
+++ app/src/main/java/com/example/elections/CurrentElection.java
@@ -0,0 +1,14 @@
+package com.example.elections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentElection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_election);
+    }
+}
